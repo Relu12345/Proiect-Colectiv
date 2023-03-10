76f3d265f4bb6006faa398d449f1b4fac7bd4032
Relu12345 <euisjmk@gmail.com>
2023-03-10 19:54:43 +0200

    modificari majore snake

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Snake : MonoBehaviour
 {
     public int xSize, ySize; 
-    public GameObject block; 
+    public GameObject block, block_food; 
 
-    GameObject head; 
-    public Material headMaterial, tailMaterial; 
-    List<GameObject> tail; 
+    GameObject head, last_tail;
+    List<GameObject> tail;
+    public Sprite blockSprite, appleSprite, bodySprite;
+    public Sprite[] headSprites, tailSprites;
+    private Vector3 oldPosition;
 
     Vector2 dir;
 
@@ -38,9 +43,10 @@ public class Snake : MonoBehaviour
         timeBetweenMovements = 0.5f;
         dir = Vector2.right;
         createGrid();
-        createPlayer(); 
+        createPlayer();
         spawnFood(); 
         block.SetActive(false);
+        block_food.SetActive(false);
         isAlive = true;
     }
 
@@ -67,31 +73,36 @@ public class Snake : MonoBehaviour
         while(containedInSnake(spawnPos)){
             spawnPos = getRandomPos();
         }
-        food = Instantiate(block);
+        food = Instantiate(block_food);
+        food.GetComponent<SpriteRenderer>().sprite = appleSprite;
         food.transform.position = new Vector3(spawnPos.x, spawnPos.y, 0);
         food.SetActive(true);
     }
 
     private void createPlayer(){
         head = Instantiate(block) as GameObject; 
-        head.GetComponent<MeshRenderer>().material = headMaterial;
+        head.GetComponent<SpriteRenderer>().sprite = headSprites[3];
         tail = new List<GameObject>(); 
     }
 
     private void createGrid(){
         for(int x = 0; x <= xSize; x++){
             GameObject borderBottom = Instantiate(block) as GameObject; 
+            borderBottom.GetComponent<SpriteRenderer>().sprite = blockSprite;
             borderBottom.GetComponent<Transform>().position = new Vector3(x-xSize/2, -ySize/2, 0);
 
-            GameObject borderTop = Instantiate(block) as GameObject; 
+            GameObject borderTop = Instantiate(block) as GameObject;
+            borderTop.GetComponent<SpriteRenderer>().sprite = blockSprite;
             borderTop.GetComponent<Transform>().position = new Vector3(x-xSize/2, ySize-ySize/2, 0);
         }
 
         for(int y = 0; y <= ySize; y++){
             GameObject borderRight = Instantiate(block) as GameObject;
+            borderRight.GetComponent<SpriteRenderer>().sprite = blockSprite;
             borderRight.GetComponent<Transform>().position = new Vector3(-xSize/2, y-(ySize/2), 0); 
 
             GameObject borderLeft = Instantiate(block) as GameObject;
+            borderLeft.GetComponent<SpriteRenderer>().sprite = blockSprite;
             borderLeft.GetComponent<Transform>().position = new Vector3(xSize-(xSize/2), y-(ySize/2), 0); 
         }
     }
@@ -110,14 +121,61 @@ public class Snake : MonoBehaviour
         points.enabled = false;
     }
 
+    private void Reset()
+    {
+
+    }
+
     public void restart(){
         SceneManager.LoadScene("joculet 2d");
+    }
+
+    public void change_head(Vector3 oldPosition, Vector3 newPosition)
+    {
+        if (oldPosition.x == newPosition.x - 1 && oldPosition.y == newPosition.y)
+            head.GetComponent<SpriteRenderer>().sprite = headSprites[3];
+        else if (oldPosition.x == newPosition.x + 1 && oldPosition.y == newPosition.y)
+            head.GetComponent<SpriteRenderer>().sprite = headSprites[2];
+        else if (oldPosition.x == newPosition.x && oldPosition.y == newPosition.y - 1)
+            head.GetComponent<SpriteRenderer>().sprite = headSprites[1];
+        else if (oldPosition.x == newPosition.x && oldPosition.y == newPosition.y + 1)
+            head.GetComponent<SpriteRenderer>().sprite = headSprites[0];
+        head.GetComponent<SpriteRenderer>().sortingLayerName = "Over";
+    }
+
+    public void change_tail(Vector3 oldPosition, Vector3 newPosition, int ok)
+    {
+        if (ok == 1)
+        {
+                if (oldPosition.x == newPosition.x - 1 && oldPosition.y == newPosition.y)
+                   last_tail.GetComponent<SpriteRenderer>().sprite = tailSprites[2];
+                else if (oldPosition.x == newPosition.x + 1 && oldPosition.y == newPosition.y)
+                    last_tail.GetComponent<SpriteRenderer>().sprite = tailSprites[3];
+                else if (oldPosition.x == newPosition.x && oldPosition.y == newPosition.y - 1)
+                    last_tail.GetComponent<SpriteRenderer>().sprite = tailSprites[0];
+                else if (oldPosition.x == newPosition.x && oldPosition.y == newPosition.y + 1)
+                    last_tail.GetComponent<SpriteRenderer>().sprite = tailSprites[1];
+                last_tail.GetComponent<SpriteRenderer>().sortingLayerName = "Behind";
+        }
+        else
+        {
+            if (oldPosition.x == newPosition.x - 1 && oldPosition.y == newPosition.y)
+                head.GetComponent<SpriteRenderer>().sprite = tailSprites[2];
+            else if (oldPosition.x == newPosition.x + 1 && oldPosition.y == newPosition.y)
+                head.GetComponent<SpriteRenderer>().sprite = tailSprites[3];
+            else if (oldPosition.x == newPosition.x && oldPosition.y == newPosition.y - 1)
+                head.GetComponent<SpriteRenderer>().sprite = tailSprites[0];
+            else if (oldPosition.x == newPosition.x && oldPosition.y == newPosition.y + 1)
+                head.GetComponent<SpriteRenderer>().sprite = tailSprites[1];
+            head.GetComponent<SpriteRenderer>().sortingLayerName = "Behind";
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(enableTimer)
+        
+        if (enableTimer)
         {
             ElapsedTime -= Time.deltaTime;
             if (ElapsedTime < 0)
@@ -162,25 +220,45 @@ public class Snake : MonoBehaviour
                 newTile.SetActive(true);
                 newTile.transform.position = food.transform.position;
                 DestroyImmediate(food);
-                head.GetComponent<MeshRenderer>().material = tailMaterial;
-                tail.Add(head); 
+                if (tail.Count == 0)
+                    change_tail(newPosition, oldPosition, 0);
+                else {
+                    head.GetComponent<SpriteRenderer>().sprite = bodySprite;
+                    head.GetComponent<SpriteRenderer>().sortingLayerName = "Middle";
+                }
+                tail.Add(head);
                 head = newTile;
-                head.GetComponent<MeshRenderer>().material = headMaterial;
+                change_head(oldPosition, newPosition);
                 spawnFood();
                 points.text = "Points: " + tail.Count;
             } else {
-                if(tail.Count == 0){
+                if (tail.Count == 0) {
                     head.transform.position = newPosition;
+                    change_head(oldPosition, newPosition);
                 } else {
-                    head.GetComponent<MeshRenderer>().material = tailMaterial;
-                    tail.Add(head); 
+                    head.GetComponent<SpriteRenderer>().sprite = bodySprite;
+                    head.GetComponent<SpriteRenderer>().sortingLayerName = "Middle";
+                    tail.Add(head);
+                    last_tail = tail[1];
+                    if (tail.Count == 2) {
+                        change_tail(newPosition, oldPosition, 1);
+                    }
+                    else if (tail.Count > 2)
+                        change_tail(tail[2].transform.position, last_tail.transform.position, 1);
+                    else
+                        change_tail(oldPosition, last_tail.transform.position, 1);
+                    if (tail.Count - 2 != 0 && tail.Count > 2)
+                        if(tail.Count - 2 != 1) {
+                            tail[tail.Count - 2].GetComponent<SpriteRenderer>().sprite = bodySprite;
+                            tail[tail.Count - 2].GetComponent<SpriteRenderer>().sortingLayerName = "Middle";
+                        }
                     head = tail[0];
-                    head.GetComponent<MeshRenderer>().material = headMaterial;
+                    change_head(oldPosition, newPosition);
                     tail.RemoveAt(0);
                     head.transform.position = newPosition;
                 }
             }
-
+            oldPosition = newPosition;
         }
         
     }
